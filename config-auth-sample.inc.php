33efe3be1dd1d45981db66cae1014bac23de7bd6
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2015-09-25 15:56:59 +0200

    add config-auth-sample.inc.php

---
+++ config-auth-sample.inc.php
@@ -0,0 +1,24 @@
+<?php
+
+$LEVEL1_FILTER = "(supannEntiteAffectation=XXXX*)";
+$LEVEL2_FILTER = "(supannEntiteAffectation=XXXXX)";
+
+
+$LDAP_CONNECT = 
+  array(
+	'HOST' =>  'ldap://ldap',
+	'BIND_DN' => 'cn=xxx,ou=admin,dc=univ,dc=fr',
+	'BIND_PASSWORD' => 'xxx'
+	);
+$LDAP_CONNECT_LEVEL1 = 
+  array(
+	'HOST' =>  'ldap://ldap',
+	'BIND_DN' => 'cn=xxx,ou=admin,dc=univ,dc=fr',
+	'BIND_PASSWORD' => 'xxx'
+	);
+$LDAP_CONNECT_LEVEL2 = 
+  array(
+	'HOST' =>  'ldap://ldap',
+	'BIND_DN' => 'cn=xxx,ou=admin,dc=univ,dc=fr',
+	'BIND_PASSWORD' => 'xxx'
+	);
