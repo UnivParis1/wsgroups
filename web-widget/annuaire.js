195b461970f91e71469caa82e3fcb93f923c5f7c
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2015-10-15 13:03:12 +0200

    annuaire: make it clear when a student is not "member"

--- web-widget/annuaire.js
+++ web-widget/annuaire.js
@@ -459,6 +459,8 @@ function compute_Affiliation(info, showExtendedInfo) {
 	var other = arraySubstraction(info.eduPersonAffiliation, notWanted);
 	var fv = formatValues(valnames, other);
 	if (fv) jqueryAppendMany(Affiliation, [" (secondaires: ", fv, ")"]);
+	if (info.eduPersonPrimaryAffiliation === 'student' && $.inArray('member', info.eduPersonAffiliation) === -1)
+	    Affiliation.appendText(" (non membre)");
     }
     return Affiliation;
 }
