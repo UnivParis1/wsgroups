a483d81f292b50d2c08e8cee5b3238d4bc7b2f50
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2016-06-30 19:17:35 +0200

    adapt test ProlongationENT url

--- web-widget/userinfo/userinfo.js
+++ web-widget/userinfo/userinfo.js
@@ -8,7 +8,7 @@ var lastLoginsUrl = baseURL + '/userLastLogins';
 var moreInfoUrl = baseURL + '/userMoreInfo';
 var helpUrl = 'https://dsidoc.univ-paris1.fr/doku.php?id=refi:userinfo-web#HELP_ID';
 helpUrl = 'https://idp.univ-paris1.fr/idp/profile/Shibboleth/SSO?shire=https://dsidoc.univ-paris1.fr/shibboleth/Shibboleth.sso/SAML/POST&target=' + escape(helpUrl) + '&providerId=https://dsidoc.univ-paris1.fr';
-var impersonateUrl = 'https://uportal3-test.univ-paris1.fr/ProlongationENT/impersonate.html';
+var impersonateUrl = 'https://ent-test.univ-paris1.fr/ProlongationENT/impersonate.html';
 var apogeeStudentDetailUrl = 'https://apogee.univ-paris1.fr/up1/jsp/detail_etudiant.jsp?config=apoprod&cod_etu=';
 var photoLdapUrl = 'https://photo-ldap.univ-paris1.fr/ldap.php';
 var grouperUrl = 'https://grouper-test.univ-paris1.fr/grouper/grouperUi/app/UiV2Main.index?operation=UiV2Group.viewGroup&membershipType=immediate&groupName=';
