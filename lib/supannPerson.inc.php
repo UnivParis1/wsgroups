fc11ed2c83f8fc0e75dd13115ae6557aff9ae8c2
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2017-06-30 13:19:51 +0200

    when searching first for users having supannRoleGenerique, also search for non supann roles (eg: "{UAI:0751717J:HARPEGE.FCSTR}529")

--- lib/supannPerson.inc.php
+++ lib/supannPerson.inc.php
@@ -116,7 +116,7 @@ function people_filters($token, $restriction = [], $allowInvalidAccounts = false
     
     if ($token === '') {
         $l[] = '(supannRoleGenerique={SUPANN}D*)'; // important people first!
-        $l[] = '(supannRoleGenerique={SUPANN}*)'; // then other important people
+        $l[] = '(supannRoleGenerique=*)'; // then other important people
         $l[] = ''; // then the rest
     } else if (preg_match('/(.*?)@(.*)/', $token, $matches)) {
         $l[] = "(|(mail=$token)(&(uid=$matches[1])(mail=*@$matches[2])))";
