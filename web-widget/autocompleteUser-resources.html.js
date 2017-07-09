05602d7fe2f26a02d8bf99ca4db33d99ce2723a5
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2017-07-09 23:44:16 +0200

    do not use document.write to load js files (banned by Chrome)

--- web-widget/autocompleteUser-resources.html.js
+++ web-widget/autocompleteUser-resources.html.js
@@ -1,1 +1,12 @@
-html2js.php
+<?php // -*- php -*-
+
+$html = '
+<link rel="stylesheet" href="//wsgroups-test.univ-paris1.fr/web-widget/jquery-ui.css" type="text/css" media="all" />
+<link rel="stylesheet" href="//wsgroups-test.univ-paris1.fr/web-widget/ui.theme.css" type="text/css" media="all" />
+<link rel="stylesheet" href="//wsgroups-test.univ-paris1.fr/web-widget/autocompleteUser.css" type="text/css" media="all" />
+';               
+echo "document.write(" . json_encode($html) . ");";
+
+include "jquery-1.7.2.min.js";
+include "jquery-ui-1.8.21.custom.min.js";
+include "autocompleteUser.js";
--- web-widget/userinfo/resources.html.js
+++ web-widget/userinfo/resources.html.js
@@ -1,1 +1,10 @@
-../html2js.php
+<?php // -*- php -*-
+
+require "../autocompleteUser-resources.html.js";
+      
+$html = '
+<link rel="stylesheet" href="//wsgroups-test.univ-paris1.fr/web-widget/userinfo/userinfo.css" type="text/css" media="all" />
+';               
+echo "document.write(" . json_encode($html) . ");\n";
+
+include "userinfo.js";
