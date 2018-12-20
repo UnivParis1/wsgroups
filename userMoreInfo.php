3316b4ff05e27cfc63976c9091705473dbb2173b
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2018-12-20 17:08:19 +0100

    userMoreInfo: restrict access to "lists" to level2

--- userMoreInfo.php
+++ userMoreInfo.php
@@ -2,17 +2,19 @@
 
 require_once ('lib/supannPerson.inc.php');
 
-forceCASAuthentication();
-if (!isPersonMatchingFilter(GET_uid(), $LEVEL1_FILTER) &&
-    !isPersonMatchingFilter(GET_uid(), $LEVEL2_FILTER)) {
-  header("HTTP/1.0 403 Forbidden");
-  exit;
-}
-
 $uid = GET_ldapFilterSafe("uid");
 $info = GET_ldapFilterSafe_or("info", '*');
 $type = GET_or("type", "user");
 if (!in_array($type, ["user", "role"])) exit("invalid user type $type");
+
+$need_level1 = [ 'auth', 'folder', 'mailbox' ];
+$wanted_level = array_diff(explode(",", $info), $need_level1) ? 2 : 1;
+
+forceCASAuthentication();
+if (loggedUserAllowedLevel() < $wanted_level) {
+  header("HTTP/1.0 403 Forbidden");
+  exit;
+}
 
 exec("ssh wsgroups@marmite.univ-paris1.fr getinfo $type '$info' $uid", $lines);
 
