4ca54904559cd6e47e84dfea60df6c39c58f1b4a
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2021-10-14 16:26:29 +0200

    getSubGroups: allow no limit on "depth" with CAS=true

--- getSubGroups.php
+++ getSubGroups.php
@@ -5,7 +5,7 @@ if (GET_bool("CAS")) forceCASAuthentication();
 
 $key = GET_ldapFilterSafe("key");
 $attrs = explode(',', GET_or_NULL("attrs"));
-$depth = min(max(0, GET_or_NULL("depth")), 3);
+$depth = GET_uid() ? GET_or("depth", 0) : min(max(0, GET_or_NULL("depth")), 3);
 $restriction = GET_extra_group_filter_from_params();
 
 $all_groups = getSubGroups($key, $depth, $restriction, $attrs);
