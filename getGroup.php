2e5bfcd0bc42a6f220600d068bae0de5f6724419
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2023-05-09 11:11:54 +0200

    /getGroup: handle trusted ip for "roles" with supannListeRouge (GLPI UP1#146786)

--- getGroup.php
+++ getGroup.php
@@ -1,7 +1,12 @@
 <?php // -*-PHP-*-
 require_once ('lib/groups.inc.php');
 
-if (GET_bool("CAS")) forceCASAuthentication();
+if (GET_bool("CAS")) {
+    forceCASAuthentication();
+} else {
+    // may set $isTrustedIp for allowListeRouge() in attrRestrictions() for roles
+    ipTrusted();
+}
 
 $key = GET_ldapFilterSafe("key");
 $attrs = explode(',', GET_or_NULL("attrs"));
